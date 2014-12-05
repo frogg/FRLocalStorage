890d5fa0e8bee12cb97daafeca10b9766f00bfaf
Frederik Riedel <f@polarapps.de>
2014-12-05 23:29:32 +0100

    data is now stored in base64 representation

--- FRLocalStorage.m
+++ FRLocalStorage.m
@@ -38,7 +38,13 @@ static NSString *filePath;
 +(void) saveDictionaryToDisc {
     NSString *saveDataString = @"";
     for(NSString *key in dictionary) {
-        saveDataString = [NSString stringWithFormat:@"%@%@==^==^==%@\n",saveDataString,key,[dictionary objectForKey:key]];
+        
+        NSString *object = [dictionary objectForKey:key];
+        NSData *nsdata = [object dataUsingEncoding:NSUTF8StringEncoding];
+        NSString *base64Object = [nsdata base64EncodedStringWithOptions:0];
+        
+        
+        saveDataString = [NSString stringWithFormat:@"%@%@==^==^==%@\n",saveDataString,key,base64Object];
     }
     [saveDataString writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
@@ -51,7 +57,10 @@ static NSString *filePath;
         
         if([[keyValuePair componentsSeparatedByString:@"==^==^=="] count]>1) {
             NSString *key = [[keyValuePair componentsSeparatedByString:@"==^==^=="] objectAtIndex:0];
-            NSString *object = [[keyValuePair componentsSeparatedByString:@"==^==^=="] objectAtIndex:1];
+            NSString *base64Object = [[keyValuePair componentsSeparatedByString:@"==^==^=="] objectAtIndex:1];
+            NSData *nsdataFromBase64String = [[NSData alloc] initWithBase64EncodedString:base64Object options:0];
+            NSString *object = [[NSString alloc] initWithData:nsdataFromBase64String encoding:NSUTF8StringEncoding];
+            
             [dictionary setObject:object forKey:key];
         }
     }
